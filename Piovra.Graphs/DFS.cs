3323e3f4a951df07bdb7c0f72e2bd60bc7a82559
xsaiter <xsaiter.mail.ru>
2019-01-13 11:03:04 -0800

    improve

--- Piovra.Graphs/DFS.cs
+++ Piovra.Graphs/DFS.cs
@@ -3,6 +3,19 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Piovra.Graphs {
-    class DFS {
+    public class DFS<V> where V : IEquatable<V> {
+        readonly IGraph<V> _g;
+
+        public DFS(IGraph<V> g) {
+            _g = g;
+        }
+
+        public Result<V> Execute(V source) {
+            var s = _g.NodeOf(source);
+
+            var res = new Result<V>(s);
+
+            return res;
+        }
     }
 }
