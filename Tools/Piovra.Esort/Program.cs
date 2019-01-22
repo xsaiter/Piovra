f2dba4c9e94ecc172fc5338297f73ab59fa8dce8
xsaiter <xsaiter@mail.ru>
2019-01-21 22:52:08 -0800

    improve

--- Tools/Piovra.Esort/Program.cs
+++ Tools/Piovra.Esort/Program.cs
@@ -2,60 +2,76 @@
 using System.Threading.Tasks;
 
 namespace Piovra.Esort {
-
     class Program {
         static async Task Main(string[] args) {
             try {
-                var n = args.Length;
-
-                if (n == 0) {
+                if (args.Length == 0) {
                     throw new Exception("invalid number of arguments");
                 }
-
                 if (args[0] == "-g") {
-                    var cfg = new FileSort.GenerateCfg();
-
-                    var i = 1;
-                    while (i < n) {
-                        var s = args[i];
-                        if (s == "-name") {
-                            cfg.Name = args[++i];
-                        } else if (s == "-size") {
-                            cfg.Size = int.Parse(args[++i]);
-                        } else if (s == "-min") {
-                            cfg.Min = int.Parse(args[++i]);
-                        } else if (s == "-max") {
-                            cfg.Max = int.Parse(args[++i]);
-                        } else {
-                            throw new Exception($"unexpected arg: {s}");
-                        }
-                        ++i;
-                    }
-                    FileSort.GenerateSourceFile(cfg);
+                    FileSort.GenerateSourceFile(ParseGenerateCfg(args));
                 } else {
-                    var cfg = new FileSort.Cfg();
-
-                    int i = 0;
-                    while (i < n) {
-                        var s = args[i];
-                        if (s == "-src") {
-                            cfg.SrcFile = args[++i];
-                        } else if (s == "-dest") {
-                            cfg.DestFile = args[++i];
-                        } else if (s == "-msize") {
-                            cfg.MemorySize = int.Parse(args[++i]);
-                        } else {
-                            throw new Exception($"unexpected arg: {s}");
-                        }
-                        ++i;
-                    }                    
-                    await FileSort.Sort(cfg);
+                    await FileSort.Sort(ParseSortCfg(args));
                 }
                 Console.WriteLine("OK");
             } catch (Exception e) {
                 Console.WriteLine($"ERROR: {e}");
             }
+
             Console.ReadKey();
+        }
+
+        static FileSort.GenerateCfg ParseGenerateCfg(string[] args) {
+            var cfg = new FileSort.GenerateCfg();
+
+            var i = 1;
+            while (i < args.Length) {
+                var s = args[i];
+                switch (s) {
+                    case "-name":
+                        cfg.Name = args[++i];
+                        break;
+                    case "-size":
+                        cfg.Size = int.Parse(args[++i]);
+                        break;
+                    case "-min":
+                        cfg.Min = int.Parse(args[++i]);
+                        break;
+                    case "-max":
+                        cfg.Max = int.Parse(args[++i]);
+                        break;
+                    default:
+                        throw new Exception($"unexpected arg: {s}");
+                }
+                ++i;
+            }
+
+            return cfg;
+        }
+
+        static FileSort.Cfg ParseSortCfg(string[] args) {
+            var cfg = new FileSort.Cfg();
+
+            int i = 0;
+            while (i < args.Length) {
+                var s = args[i];
+                switch (s) {
+                    case "-src":
+                        cfg.SrcFile = args[++i];
+                        break;
+                    case "-dest":
+                        cfg.DestFile = args[++i];
+                        break;
+                    case "-msize":
+                        cfg.MemorySize = int.Parse(args[++i]);
+                        break;
+                    default:
+                        throw new Exception($"unexpected arg: {s}");
+                }
+                ++i;
+            }
+
+            return cfg;
         }
     }
 }
