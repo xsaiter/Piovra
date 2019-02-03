0410429ce2bd8353856575956fda00555c405585
xsaiter <xsaiter@mail.ru>
2019-02-03 11:51:25 -0800

    improve

---
+++ Piovra.Core/Numeric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Piovra {
+    public class Numeric {
+        public static bool IsPrime(int n) {
+            for (var i = 2; i * i <= n; ++i) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- Tools/Piovra.Problems/Mesh/T_01.cs
+++ Tools/Piovra.Problems/Mesh/T_01.cs
@@ -4,9 +4,17 @@ using System.Text;
 
 namespace Piovra.Problems.Mesh {
     public class T_01_A_Primes {
+        public List<int> Execute(int m, int n) {
+            var res = new List<int>();
+            for(var i = m; i <= n; ++i) {
+                if (Numeric.IsPrime(i)) {
+                    res.Add(i);
+                }
+            }
+            return res;
+        }
     }
 
-    public class T_01_A_Expr {
-
+    public class T_01_A_Expr {        
     }
 }
