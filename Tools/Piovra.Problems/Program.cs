71e3391449f1cf4f32e3b2d45a915deb6a6f6327
xsaiter <xsaiter@mail.ru>
2019-04-14 23:30:13 +0300

    add project problems

---
+++ Tools/Piovra.Problems/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Piovra;
+
+namespace Piovra.Problems {
+    class Program {
+        static void Main(string[] args) {
+            var s = "asd";
+            var empty = s.Empty();
+        }
+    }
+}
