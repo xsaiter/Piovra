58f176fe6c19d41b81ea02af1febde1ec135789b
xsaiter <xsaiter@mail.ru>
2019-06-21 15:30:38 -0700

    to 2.2

--- Tools/Piovra.Problems/Program.cs
+++ Tools/Piovra.Problems/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using Piovra;
 
 namespace Piovra.Problems {
+    public class Watch {
+        readonly List<int> x = new List<int>();        
+    }
     class Program {
         static void Main(string[] args) {
             var s = "asd";
