6cf6bb521da53f4420786d7abcb313c0c12e6c79
xsaiter <xsaiter@mail.ru>
2020-07-25 22:49:37 +0300

    btree impl

--- Piovra.Ds/BTree.cs
+++ Piovra.Ds/BTree.cs
@@ -14,35 +14,64 @@ namespace Piovra.Ds {
         }
 
         public int Degree { get; }
-        public Node Root { get; set; }
-        public int H { get; set; }
+        public Node Root { get; private set; }
+        public int H { get; private set; }
 
         public Entry Search(K key) {
-            return SearchRec(Root, key);
+            return SearchImpl(Root, key);
         }
 
-        Entry SearchRec(Node x, K key) {
+        Entry SearchImpl(Node x, K key) {
             var pos = x.Entries.TakeWhile(_ => key.Ge(_.Key)).Count();
             if (pos < x.NEntries && x.Entries[pos].Key.Eq(key)) {
                 return x.Entries[pos];
             }
-            return x.IsLeaf ? null : SearchRec(x.Children[pos], key);
+            return x.IsLeaf ? null : SearchImpl(x.Children[pos], key);
         }
 
         public void Add(K key, V value) {
-            if (!Root.IsMax()) {
-                InsertNonFull(Root, key, value);
+            if (!Root.HasReachedMaxEntries) {
+                InsertNonFull(this.Root, key, value);
             } else {
-                ++H;
+                var oldRoot = Root;
+                Root = new Node(Degree);
+                Root.Children.Add(oldRoot);
+                SplitChild(Root, 0, oldRoot);
+                InsertNonFull(Root, key, value);
+                H++;
             }
         }
 
-        void Split(Node x, K key, V value) {
+        private void SplitChild(Node parent, int index, Node toSplit) {
+            var newNode = new Node(Degree);
 
+            parent.Entries.Insert(index, toSplit.Entries[Degree - 1]);
+            parent.Children.Insert(index + 1, newNode);
+
+            newNode.Entries.AddRange(toSplit.Entries.GetRange(Degree, Degree - 1));
+
+            toSplit.Entries.RemoveRange(Degree - 1, Degree);
+
+            if (!toSplit.IsLeaf) {
+                newNode.Children.AddRange(toSplit.Children.GetRange(Degree, Degree));
+                toSplit.Children.RemoveRange(Degree, Degree);
+            }
         }
 
-        void InsertNonFull(Node x, K key, V value) {
-
+        private void InsertNonFull(Node node, K key, V value) {
+            var positionToInsert = node.Entries.TakeWhile(entry => key.CompareTo(entry.Key) >= 0).Count();
+            if (node.IsLeaf) {
+                node.Entries.Insert(positionToInsert, new Entry { Key = key, Value = value });                
+            } else {
+                var child = node.Children[positionToInsert];
+                if (child.HasReachedMaxEntries) {
+                    SplitChild(node, positionToInsert, child);
+                    if (key.CompareTo(node.Entries[positionToInsert].Key) > 0) {
+                        positionToInsert++;
+                    }
+                }
+                InsertNonFull(node.Children[positionToInsert], key, value);
+            }            
         }
 
         public class Node {
@@ -58,8 +87,8 @@ namespace Piovra.Ds {
             public int NEntries => Entries.Count;
             public bool IsLeaf { get; set; }
 
-            public bool IsMax() => NEntries == 2 * Degree - 1;
-            public bool IsMin() => NEntries == Degree - 1;
+            public bool HasReachedMaxEntries => Entries.Count == 2 * Degree - 1;
+            public bool HasReachedMinEntries => Entries.Count == Degree - 1;
         }
 
         public class Entry {
