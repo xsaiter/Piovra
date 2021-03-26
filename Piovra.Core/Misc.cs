7fc9f721e8aecbb2582c18d4dda2f478448666b2
xsaiter <xsaiter@mail.ru>
2021-03-26 21:48:11 +0300

    improve

--- Piovra.Core/Misc.cs
+++ Piovra.Core/Misc.cs
@@ -12,4 +12,13 @@ namespace Piovra {
             }
         }
     }
+
+    public class ASSERT {
+        public static void True(bool condition, string message = null) {
+            if (!condition) {
+                throw new Exception(message);
+            }
+        }
+        public static void False(bool condition, string message = null) => True(!condition, message);
+    }
 }
