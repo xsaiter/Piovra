d64bd360a000997eb3c7d349f1f68030e53fd329
xsaiter <xsaiter@mail.ru>
2026-04-11 12:05:20 +0300

    improve

--- Piovra.Core/Requires.cs
+++ Piovra.Core/Requires.cs
@@ -5,8 +5,9 @@ namespace Piovra;
 
 public static class Requires {
     public const string ASSERTION_FAILED = "Assertion failed";
-    public const string DEFAULT_NAME = "";
-    public const string DEFAULT_DETAILS = "";
+    public const string DEFAULT_NAME = EMPTY;
+    public const string DEFAULT_DETAILS = EMPTY;
+    public const string EMPTY = "";
 
     public static void True([DoesNotReturnIf(false)] bool condition, string message = ASSERTION_FAILED) {
         if (!condition) {
@@ -14,25 +15,35 @@ public static class Requires {
         }
     }
 
-    [return: NotNull]
-    public static T AsNotNull<T>([NotNull] T? value,
-        [CallerArgumentExpression(nameof(value))] string name = DEFAULT_NAME) where T : class {
-
-        True(value is not null, $"{name} must not be null");
-        return value;
-    }
-
-    [return: NotNull]
-    public static string AsNotNullOrEmpty(string value,
-        [CallerArgumentExpression(nameof(value))] string name = DEFAULT_NAME) {
-
-        True(value.NonEmpty(), $"{name} must not be null or empty");
-        return value;
-    }
-
     public static void NotNull<T>([NotNull] T value,
         string details = DEFAULT_DETAILS, [CallerArgumentExpression(nameof(value))] string name = DEFAULT_NAME) {
 
-        True(value is not null, $"{name} must not be null, {nameof(details)}: {details}");
+        True(value is not null, PrettyMessage(name, details, $"must not be null"));
+    }
+
+    public static void NotNullOrEmpty(string value,
+        string details = DEFAULT_DETAILS, [CallerArgumentExpression(nameof(value))] string name = DEFAULT_NAME) {
+
+        True(value.NonEmpty(), PrettyMessage(name, details, "must not be null or empty"));
+    }
+
+    [return: NotNull]
+    public static T AsNotNull<T>([NotNull] T? value, string details = DEFAULT_DETAILS,
+       [CallerArgumentExpression(nameof(value))] string name = DEFAULT_NAME) where T : class {
+
+        NotNull(value: value, details: details, name: name);
+        return value;
+    }
+
+    [return: NotNull]
+    public static string AsNotNullOrEmpty(string value, string details = DEFAULT_DETAILS,
+        [CallerArgumentExpression(nameof(value))] string name = DEFAULT_NAME) {
+
+        NotNullOrEmpty(value: value, details: details, name: name);
+        return value;
+    }
+
+    static string PrettyMessage(string name, string details, string requirements) {
+        return $"{name} {requirements}, {nameof(details)}: {details}";
     }
 }
